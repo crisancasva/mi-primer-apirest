2bb815b796a86df4e8d2ac6ebfdf0d911e7dddde
cristhian.castano <crisan1988@hotmail.com>
2025-06-13 00:41:29 -0500

    segundo end point

--- src/main/java/com/crisan/controller/ApiController.java
+++ src/main/java/com/crisan/controller/ApiController.java
@@ -1,6 +1,7 @@
 package com.crisan.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -14,6 +15,15 @@ public class ApiController {
     public Map<String, String> saludar(){
         Map<String, String> json = new HashMap<>();
         json.put("Saludo", "Hola Bienvenido a mi primer api REST");
+
+        return json;
+    }
+    // 2. Endpoint saludar personalizado
+
+    @GetMapping("/saludar/{nombre}")
+    public Map<String, String> saludarNombre(@PathVariable String nombre){
+        Map<String, String> json = new HashMap<>();
+        json.put("Saludo", "Hola Bienvenido " + nombre + " a mi primer api REST");
 
         return json;
     }
