4bfa9f2ff4ed838de54afd766cbedf170b018f2f
cristhian.castano <crisan1988@hotmail.com>
2025-06-13 00:50:13 -0500

    segundo end point

--- src/main/java/com/crisan/controller/ApiController.java
+++ src/main/java/com/crisan/controller/ApiController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,6 +25,16 @@ public class ApiController {
     public Map<String, String> saludarNombre(@PathVariable String nombre){
         Map<String, String> json = new HashMap<>();
         json.put("Saludo", "Hola Bienvenido " + nombre + " a mi primer api REST");
+
+        return json;
+    }
+    // 3. Endpoint mostrar fechas actual y dentro de 875 dias
+
+    @GetMapping("/fechas")
+    public Map<String,String> fechaActual(){
+        Map<String, String> json = new HashMap<>();
+        json.put("Fecha Actual: ", LocalDate.now().toString());
+        json.put("Fecha en 875 dias: : ", LocalDate.now().plusDays(875).toString());
 
         return json;
     }
