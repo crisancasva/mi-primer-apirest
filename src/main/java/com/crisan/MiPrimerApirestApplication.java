f566a080c03564caeddc356bbe2bda117ae311de
cristhian.castano <crisan1988@hotmail.com>
2025-06-12 23:50:09 -0500

    Se agrega el proyecto

---
+++ src/main/java/com/crisan/MiPrimerApirestApplication.java
@@ -0,0 +1,13 @@
+package com.crisan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiPrimerApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiPrimerApirestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crisan/MiPrimerApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crisan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiPrimerApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
